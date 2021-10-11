6c48aa030187c96f80b3ec2161034fcd8b15cdfb
Daniel Kelly <me@danielkelly.io>
2021-10-11 09:47:30 -0500

    added div as root element of DestinationShow page

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <section v-if="destination" class="destination">
       <h1>{{destination.name}}</h1>
       <GoBack />
@@ -25,6 +26,7 @@
       </div>
      <router-view /> 
   </section>
+</div>
 </template>
 
 <script>
